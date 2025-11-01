66e9aa4f343f64e41c7da3297f6eecf2912bced6
Arya S <aryaofficial12a@gmail.com>
2025-11-01 11:39:23 +0530

    Add files via upload

---
+++ pattern_3.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number of rows you need="))
+for i in range (a,0,-1):
+    for j in range(i,a+1):
+        print(j, end=" ")
+    print('\n')
---
+++ pattern_5.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+c=1
+for i in range(1,a+1,2):
+    for j in range(c):
+         print(i,end=" ")
+    print()
+    c=c+1
